7eaf3844308f423122c8a8b83d69861f3bf7bb70
Gold Israel <techiegold101@gmail.com>
2024-07-29 11:44:56 +0100

    complete image upload functionality

--- frontend/src/firebabse.js
+++ frontend/src/firebabse.js
@@ -1,3 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "my-estate-app-901b8.firebaseapp.com",
+//   projectId: "my-estate-app-901b8",
+//   storageBucket: "my-estate-app-901b8.appspot.com",
+//   messagingSenderId: "892409629302",
+//   appId: "1:892409629302:web:6028eded2db22ff59c2d57"
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,11 +24,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "my-estate-app-901b8.firebaseapp.com",
-  projectId: "my-estate-app-901b8",
-  storageBucket: "my-estate-app-901b8.appspot.com",
-  messagingSenderId: "892409629302",
-  appId: "1:892409629302:web:6028eded2db22ff59c2d57"
+  authDomain: "techiegold-estate.firebaseapp.com",
+  projectId: "techiegold-estate",
+  storageBucket: "techiegold-estate.appspot.com",
+  messagingSenderId: "487322816641",
+  appId: "1:487322816641:web:90281b1bb3b083bf413d80"
 };
 
 // Initialize Firebase
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,15 +1,65 @@
 import { useSelector } from 'react-redux';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebabse';
 
 export default function Profile() {
   const { currentUser } = useSelector((state)=> state.user);
   const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [imageUploadProgress, setImageUploadProgress] = useState(0);
+  const [imageUploadError, setimageUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  
+
+
+  useEffect(() => {
+    if (file){
+      handleFileUpload(file)
+    }
+  }, [file])
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() +  file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageUploadProgress(Math.round(progress));
+      },
+      (error) => {
+        setimageUploadError(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((getDownloadURL) => 
+            setFormData({...formData, avatar: getDownloadURL}));
+      }
+    )
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className='flex flex-col gap-4'>
-        <input type="file" ref={fileRef} hidden accept='image/*'/>
-        <img onClick={()=>fileRef.current.click()} className='w-24 h-24 rounded-full object-cover self-center mt-2 cursor-pointer' src={currentUser.avatar} alt="profile photo" />
+        <input onChange={(e) =>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*'/>
+        <img
+          className='w-24 h-24 rounded-full object-cover self-center mt-2 cursor-pointer'
+          onClick={()=>fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar} alt="profile photo" />
+
+        <p className='text-sm self-center'>
+          { imageUploadError ? (
+              <span className='text-red-700'>Could not upload image, try again!</span>
+            ): imageUploadProgress > 0 && imageUploadProgress < 100 ? (
+              <span className='text-slate-500'>{`Uploading ${imageUploadProgress}%`}</span>
+            ) :  imageUploadProgress === 100 ? (
+              <span className='text-green-700'>Image uploaded successfully</span>
+            ) : (' ')     
+          }
+        </p>
+        
         <input className='border p-3 rounded-lg' type="text" placeholder='Username' id='username'/>
         <input className='border p-3 rounded-lg' type="email" placeholder='email' id='email' />
         <input className='border p-3 rounded-lg' type="password" placeholder='Password' id='password' />
